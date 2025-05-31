70af7acbf42309943b70d657e6e4da6eda15a9a6
ZOJunnn <106294821+ZOJunnn@users.noreply.github.com>
2025-05-31 21:32:06 +0800

    Add files via upload

---
+++ model/__init__.py
@@ -0,0 +1,9 @@
+from .pointnet import PointNetCls, feature_transform_reguliarzer
+from .pointnet2 import PointNet2ClsSsg
+from .dgcnn import DGCNN
+from .pointconv import PointConvDensityClsSsg
+# from .pn_feature import PNFeature
+# from .pn2_feature import PN2Feature
+# from .dgcnn_feature import DGCNNFeature
+# from .pconv_feature import PConvFeature
+
